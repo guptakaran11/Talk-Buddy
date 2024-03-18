6d0b0e6f25295d39605eaeb1d696555a9ba347b4
guptakaran11 <gupta.karan1162@gmail.com>
2024-03-18 13:56:19 +0530

    form field customization

--- lib/View/pages/loginPage.dart
+++ lib/View/pages/loginPage.dart
@@ -8,8 +8,59 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late double height;
+  late double width;
+
+  final loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: width * 0.03,
+          vertical: height * 0.02,
+        ),
+        height: height * 0.98,
+        width: width * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: height * 0.10,
+              child: const Text(
+                'Talk Buddy',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            loginForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget loginForm() {
+    return SizedBox(
+      height: height * 0.18,
+      child: Form(
+        key: loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/View/widgets/inputFields.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, file_names
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final Function(String) onSaved;
+  final String regExp;
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextFormField({
+    super.key,
+    required this.onSaved,
+    required this.regExp,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onSaved: (value) => onSaved(value!),
+      cursorColor: Colors.white,
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+      obscureText: obscureText,
+    );
+  }
+}
