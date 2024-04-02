7fcb20ff12624829cee0160e3fe577702048a3c0
guptakaran11 <gupta.karan1162@gmail.com>
2024-04-02 12:58:06 +0530

    small change fix

--- lib/View/pages/chatAndUserPages/userScreen.dart
+++ lib/View/pages/chatAndUserPages/userScreen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:talkbuddy/Controller/provider/authenticationProvider.dart';
 import 'package:talkbuddy/Controller/provider/userPageProvider.dart';
